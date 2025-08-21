67575abba1494894a6451479714297d6e41a1e2f
Sotatek-DongBui <dong.bui@sotatek.com>
2025-08-21 13:12:00 +0700

    add problem 228

---
+++ intervals/228-summary-ranges.go
@@ -0,0 +1,27 @@
+func summaryRanges(nums []int) []string {
+	res := make([]string, 0)
+
+	start := 0
+
+	for start < len(nums) {
+		end := start
+		for end < len(nums)-1 {
+			if nums[end+1]-nums[end] != 1 {
+				break
+			} else {
+				end++
+			}
+		}
+
+		if end != start {
+			res = append(res, strconv.Itoa(nums[start])+"->"+strconv.Itoa(nums[end]))
+		} else {
+			res = append(res, strconv.Itoa(nums[start]))
+		}
+		start = end + 1
+
+	}
+
+	return res
+
+}

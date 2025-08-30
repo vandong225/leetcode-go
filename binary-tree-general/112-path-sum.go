3ec8b3f06c416380a2265d4fb497de1c8d5fe81f
Sotatek-DongBui <dong.bui@sotatek.com>
2025-08-30 13:33:33 +0700

    add problem 112

---
+++ binary-tree-general/112-path-sum.go
@@ -0,0 +1,21 @@
+package binarytreegeneral
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+
+	if root.Val == targetSum && root.Left == nil && root.Right == nil {
+		return true
+	}
+
+	return hasPathSum(root.Left, targetSum-root.Val) || hasPathSum(root.Right, targetSum-root.Val)
+}

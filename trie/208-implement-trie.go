aea264d554a01ee19620e072c339101d10796f56
Sotatek-DongBui <dong.bui@sotatek.com>
2025-09-05 10:08:03 +0700

    add problem 208

---
+++ trie/208-implement-trie.go
@@ -0,0 +1,50 @@
+package trie
+
+const (
+	partial_inserted = 1
+	fully_inserted   = 2
+)
+
+type Trie struct {
+	Dict map[string]int
+}
+
+func Constructor() Trie {
+	return Trie{Dict: make(map[string]int)}
+}
+
+func (this *Trie) Insert(word string) {
+	str := ""
+
+	for _, ch := range word {
+		str += string(ch)
+		if _, ok := this.Dict[str]; !ok {
+			this.Dict[str] = partial_inserted
+
+		}
+	}
+
+	this.Dict[word] = fully_inserted
+}
+
+func (this *Trie) Search(word string) bool {
+	v, ok := this.Dict[word]
+	if !ok {
+		return false
+	}
+
+	return v == fully_inserted
+}
+
+func (this *Trie) StartsWith(prefix string) bool {
+	_, ok := this.Dict[prefix]
+	return ok
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Insert(word);
+ * param_2 := obj.Search(word);
+ * param_3 := obj.StartsWith(prefix);
+ */

28397a0f92b3585fe9e225d629d4998e14e4ba64
Sotatek-DongBui <dong.bui@sotatek.com>
2025-09-09 12:01:49 +0700

    add problem 53

---
+++ kadane/53-maximum-subarray.go
@@ -0,0 +1,16 @@
+package kadane
+
+func maxSubArray(nums []int) int {
+	cur, best := nums[0], nums[0]
+	for i := 1; i < len(nums); i++ {
+		if cur+nums[i] > nums[i] {
+			cur += nums[i]
+		} else {
+			cur = nums[i]
+		}
+		if cur > best {
+			best = cur
+		}
+	}
+	return best
+}

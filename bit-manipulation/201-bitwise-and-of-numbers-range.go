4397679cd4605b0b0cc49717204d842dcdd41e5b
Sotatek-DongBui <dong.bui@sotatek.com>
2025-09-14 10:54:08 +0700

    add problem 210

---
+++ bit-manipulation/201-bitwise-and-of-numbers-range.go
@@ -0,0 +1,12 @@
+package bitmanipulation
+
+func rangeBitwiseAnd(left int, right int) int {
+	shift := 0
+
+	for left < right {
+		left >>= 1
+		right >>= 1
+		shift++
+	}
+	return right << shift
+}

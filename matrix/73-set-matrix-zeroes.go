62fe785398b546d0cdab0685434bfe86690fd19d
Sotatek-DongBui <dong.bui@sotatek.com>
2025-09-21 11:10:56 +0700

    add problem 73

---
+++ matrix/73-set-matrix-zeroes.go
@@ -0,0 +1,23 @@
+package matrix
+
+func setZeroes(matrix [][]int) {
+	m, n := len(matrix), len(matrix[0])
+	rows, cols := make([]bool, m), make([]bool, n)
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if matrix[i][j] == 0 {
+				rows[i] = true
+				cols[j] = true
+			}
+		}
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if rows[i] || cols[j] {
+				matrix[i][j] = 0
+			}
+		}
+	}
+}
